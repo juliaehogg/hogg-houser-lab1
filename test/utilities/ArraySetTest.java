5da1402b09f64b5ef7526ef802a3391296b0c9fc
juliaehogg <122399945+juliaehogg@users.noreply.github.com>
2023-01-17 19:21:17 -0500

    testing for add element methods

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -100,41 +100,6 @@ class ArraySetTest
 		assertEquals(3, three.size());
 		
 		
-		// STRING TEST
-		ArraySet<String> four = new ArraySet<String>();
-		assertEquals(0, four.size());
-
-		four.add("hello ");
-		assertEquals(1, four.size());
-
-		four.add("here");
-		four.add("here");
-		four.add("here");
-		assertEquals(2, four.size());
-
-		four.add("here");
-		four.add("is a ");
-		four.add("butterfly");
-		assertEquals(4, four.size());
-
-		four.clear();
-		assertEquals(0, four.size());
-
-		four.add("");
-		four.add("okay! cool <3");
-		assertEquals(2, four.size());
-		
-		four.remove("not");
-		assertEquals(2, four.size());
-		four.remove("");
-		assertEquals(1, four.size());
-		four.remove("okay! cool <3");
-		assertEquals(0, four.size());
-		
-		four.add("tricky");
-		four.add("it is the start of the Furman semester");
-		four.add("!@#$%^&*()_+{}[]:\' oh how fancy");
-		assertEquals(3, four.size());
 	}
 	
 	
@@ -169,18 +134,109 @@ class ArraySetTest
 		test.remove(6);
 		assertFalse(test.contains(6));
 		
+
+		
+		// double type
+		ArraySet<Double> test2 = new ArraySet<Double>();
+		assertFalse(test2.contains(1.1));
+		
+		// populate with values 0.1, 1.1, 2.2, ... 10.1
+		for (double d = 0.1; d < 10.2; d++)
+		{
+			test2.add(d);
+		}
+		
+		// add and make sure it added
+		assertTrue(test2.contains(2.1));
+		assertTrue(test2.contains(4.1));
+		assertTrue(test2.contains(10.1));
+		
+		
+		// values not added, not in range
+		assertFalse(test2.contains(6));
+		assertFalse(test2.contains(4.4));
+		assertFalse(test2.contains(60.5));
+		assertFalse(test2.contains(-8.1));
+		
+		// zero and null
+		assertFalse(test2.contains(0));
+		assertTrue(test2.contains(0.1));
+		assertFalse(test.contains(null));
+		
+		// remove and check
+		test2.remove(4.1);
+		assertFalse(test2.contains(4.1));
 		
 	}
 	
-	
-	
-	
-	
 	@Test
 	void testArraySet_add_elementAtIndex() {
+		// integer type
+		ArraySet<Integer> test = new ArraySet<Integer>();
+		assertFalse(test.contains(1));
+				
+		// populate array
+		for (int i = 0; i < 20; i++)
+		{
+			test.add(i, i);
+		}
+		
+		// add and make sure it added
+		assertTrue(test.contains(2));
+		assertEquals(2, test.indexOf(2));
+		assertTrue(test.contains(4));
+		assertEquals(4, test.indexOf(4));
+		assertTrue(test.contains(17));
+		assertEquals(17, test.indexOf(17));
+		
+		// values not added, not in range
+		assertFalse(test.contains(-4));
+		assertFalse(test.contains(59));
+		
+		// zero and null
+		assertTrue(test.contains(0));
+		assertEquals(0, test.indexOf(0));
+		assertFalse(test.contains(null));
+		
+		// remove and check
+		test.remove(6);
+		assertFalse(test.contains(6));
 		
 		
-		
+		// double type
+		ArraySet<Double> test2 = new ArraySet<Double>();
+		assertFalse(test2.contains(1.1));
+				
+		// populate with values 0.1, 1.1, 2.2, ... 10.1
+		for (double d = 0.1; d < 10.2; d++)
+		{
+			test2.add(d);
+		}
+				
+		// add and make sure it added
+		assertTrue(test2.contains(2.1));
+		assertEquals(2, test2.indexOf(2.1));
+		assertTrue(test2.contains(4.1));
+		assertEquals(4, test2.indexOf(4.1));
+		assertTrue(test2.contains(10.1));
+		assertEquals(10, test2.indexOf(10.1));
+				
+				
+		// values not added, not in range
+		assertFalse(test2.contains(6));
+		assertFalse(test2.contains(4.4));
+		assertFalse(test2.contains(60.5));
+		assertFalse(test2.contains(-8.1));
+				
+		// zero and null
+		assertFalse(test2.contains(0));
+		assertTrue(test2.contains(0.1));
+		assertEquals(0, test2.indexOf(0.1));
+		assertFalse(test.contains(null));
+				
+		// remove and check
+		test2.remove(4.1);
+		assertFalse(test2.contains(4.1));
 	}
 
 	
